cab4031bb2243dad18c8bfdd739c345a579528ad
Patryk K <patryk.koc44@gmail.com>
2025-05-09 12:18:23 +0200

    added test for personpaper not working for papertest

--- src/main/java/com/learning/courses/model/Paper.java
+++ src/main/java/com/learning/courses/model/Paper.java
@@ -1,12 +1,27 @@
 package com.learning.courses.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import java.util.List;
 
 public class Paper {
+
+    @NotNull
     String TypeOfPaper;
+
+    @NotEmpty
     List<Person> Authors ;
+
+    //13 cyfr
+    @Pattern(regexp = "^\\d{13}$")
     String ISBN;
+
+    @NotEmpty
     String Topic;
+    @NotEmpty
     String Content;
 
 }
+
---
+++ src/test/java/com/learning/courses/model/PersonPaperTest.java
@@ -0,0 +1,40 @@
+package com.learning.courses.model;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonPaperTest {
+
+    @Test
+    void shouldAddMultiplePapersToTutor() {
+
+    }
+    @Test
+    void shouldNotAddPaperToNoneExistingPerson() {
+
+    }
+
+    @Test
+    void shouldNotAssignPapersToStudent() {
+        //more than one
+    }
+
+    @Test
+    void shouldLinkPaperToCorrectPerson() {
+       // Paper author == person that we add our paper to
+    }
+
+    @Test
+    void shouldNotLinkPaperToIncorrectPerson() {
+        // Paper author != person that we add our paper to
+    }
+    @Test
+    void shouldAllowUpdatingPapersForAuthor() {
+
+    }
+
+    @Test
+    void shouldAllowRemovingPapersForAuthor() {
+
+    }
+
+}
